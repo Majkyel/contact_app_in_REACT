9d7433727d989cb2aa295328d98cd861aa4cfa6c
Michał Kuliś <michalkulis1993@gmail.com>
2018-07-14 01:57:15 +0200

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -10,7 +10,7 @@ var Contact = React.createClass({
                 src: 'http://icons.veryicon.com/png/Application/iOS8%20Cirtangle%20Concept/Contacts.png', alt: 'Avatar'}),
                 React.createElement('p', {className: 'contactName col-8'}, 'First name: ' + this.props.item.firstName),
                 React.createElement('p', {className: 'contactName col-8'}, 'Last name: ' + this.props.item.lastName),
-                React.createElement('a', {className: 'contactEmail col-8', href: 'Email: ' + this.props.item.email}, this.props.item.email),
+                React.createElement('a', {className: 'contactEmail col-8', href: 'Email: ' + this.props.item.email}, this.props.item.email)
             )
         )
     }
