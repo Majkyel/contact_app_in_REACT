394d135735ee60b9314d0ef701b2467cf12b7c37
Michał Kuliś <michalkulis1993@gmail.com>
2018-07-14 01:56:19 +0200

    Update elements in components folder

--- components/Contact.js
+++ components/Contact.js
@@ -6,7 +6,8 @@ var Contact = React.createClass({
     render: function() {
         return (
             React.createElement('div', {className: 'contactItem row'},
-                React.createElement('img', {className: 'contactImage col-3', src: 'http://icons.veryicon.com/png/Application/iOS8%20Cirtangle%20Concept/Contacts.png'}),
+                React.createElement('img', {className: 'contactImage col-3',
+                src: 'http://icons.veryicon.com/png/Application/iOS8%20Cirtangle%20Concept/Contacts.png', alt: 'Avatar'}),
                 React.createElement('p', {className: 'contactName col-8'}, 'First name: ' + this.props.item.firstName),
                 React.createElement('p', {className: 'contactName col-8'}, 'Last name: ' + this.props.item.lastName),
                 React.createElement('a', {className: 'contactEmail col-8', href: 'Email: ' + this.props.item.email}, this.props.item.email),
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,18 +6,24 @@ var ContactForm = React.createClass({
     render: function() {
         return (
             React.createElement('form', {className: 'contactForm'},
+                React.createElement('label', {htmlFor: 'firstname', className: 'label'}, 'First name'),
                 React.createElement('input', {
                     type: 'text',
+                    id: 'firstname',
                     placeholder: 'Firstname',
                     value: this.props.contact.firstName,
                 }),
+                React.createElement('label', {htmlFor: 'lastname', className: 'label'}, ' Last name'),
                 React.createElement('input', {
                     type: 'text',
+                    id: 'lastname',
                     placeholder: 'Lastname',
                     value: this.props.contact.lastName,
                 }),
+                React.createElement('label', {htmlFor: 'email', className: 'label'}, ' Email'),
                 React.createElement('input', {
                     type: 'email',
+                    id: 'email',
                     placeholder: 'Email',
                     value: this.props.contact.email,
                 }),
